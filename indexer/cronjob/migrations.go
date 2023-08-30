bf6835e4ca819b903661095730fb774915cce34e
Ryan Collingham <ryanc118@gmail.com>
2023-08-30 12:14:56 +0100

    add initial migration for voting cronjob state

--- indexer/cronjob/migrations.go
+++ indexer/cronjob/migrations.go
@@ -10,11 +10,21 @@ import (
 
 func init() {
 	migrations.Container.Add("2023-08-25-00-00", "Create initial state for voting cronjob", createVotingCronjobState)
+	migrations.Container.Add("2023-08-30-00-00", "Create initial state for mirror cronjob", createMirrorCronjobState)
 }
 
 func createVotingCronjobState(db *gorm.DB) error {
 	return database.CreateState(db, &database.State{
 		Name:           VotingStateName,
+		NextDBIndex:    0,
+		LastChainIndex: 0,
+		Updated:        time.Now(),
+	})
+}
+
+func createMirrorCronjobState(db *gorm.DB) error {
+	return database.CreateState(db, &database.State{
+		Name:           mirrorStateName,
 		NextDBIndex:    0,
 		LastChainIndex: 0,
 		Updated:        time.Now(),
