a994eab49ff6b5fe4b2eae05cfad8c32bc63ef4a
Marko Boben <marko.boben@gmail.com>
2023-09-18 21:08:11 +0200

    Added route to list all staking data at specific timestamp (/stakers/list)

--- services/routes/staking.go
+++ services/routes/staking.go
@@ -4,21 +4,38 @@ import (
 	"flare-indexer/database"
 	"flare-indexer/services/context"
 	"flare-indexer/services/utils"
+	"flare-indexer/utils/staking"
 	"net/http"
+	"strings"
 	"time"
 
 	"gorm.io/gorm"
 )
 
-type GetStakerRequest struct {
+type GetStakerTxRequest struct {
 	PaginatedRequest
 	NodeID  string    `json:"nodeId"`
 	Address string    `json:"address"`
 	Time    time.Time `json:"time"`
 }
 
-type GetStakerResponse struct {
+type GetStakerTxResponse struct {
 	TxIDs []string `json:"txIds"`
+}
+
+type GetStakerRequest struct {
+	PaginatedRequest
+	Time time.Time `json:"time"`
+}
+
+type GetStakerResponse struct {
+	Type           uint8    `json:"type"` // 0 = validator, 1 = delegator
+	TxID           string   `json:"txID"`
+	NodeID         string   `json:"nodeID"`
+	StartTime      int64    `json:"startTime"`
+	EndTime        int64    `json:"endTime"`
+	Weight         uint64   `json:"weight"`
+	InputAddresses []string `json:"inputAddresses"`
 }
 
 type stakerRouteHandlers struct {
@@ -32,15 +49,40 @@ func newStakerRouteHandlers(ctx context.ServicesContext) *stakerRouteHandlers {
 }
 
 func (rh *stakerRouteHandlers) listStakingTransactions(txType database.PChainTxType) utils.RouteHandler {
-	handler := func(request GetStakerRequest) (GetStakerResponse, *utils.ErrorHandler) {
+	handler := func(request GetStakerTxRequest) (GetStakerTxResponse, *utils.ErrorHandler) {
 		txIDs, err := database.FetchPChainStakingTransactions(rh.db, txType, request.NodeID,
 			request.Address, request.Time, request.Offset, request.Limit)
 		if err != nil {
-			return GetStakerResponse{}, utils.InternalServerErrorHandler(err)
+			return GetStakerTxResponse{}, utils.InternalServerErrorHandler(err)
 		}
-		return GetStakerResponse{TxIDs: txIDs}, nil
+		return GetStakerTxResponse{TxIDs: txIDs}, nil
 	}
-	return utils.NewRouteHandler(handler, http.MethodPost, GetStakerRequest{}, GetStakerResponse{})
+	return utils.NewRouteHandler(handler, http.MethodPost, GetStakerTxRequest{}, GetStakerTxResponse{})
+}
+
+func (rh *stakerRouteHandlers) listStakers() utils.RouteHandler {
+	handler := func(request GetStakerRequest) ([]GetStakerResponse, *utils.ErrorHandler) {
+		stakerTxData, err := database.FetchPChainStakingData(rh.db, request.Time, request.Offset, request.Limit)
+		if err != nil {
+			return nil, utils.InternalServerErrorHandler(err)
+		}
+		stakers := make([]GetStakerResponse, len(stakerTxData))
+		for i, tx := range stakerTxData {
+			txType, _ := staking.GetTxType(tx.Type) // ignore error, query filters only valid types
+
+			stakers[i] = GetStakerResponse{
+				Type:           txType,
+				TxID:           *tx.TxID,
+				NodeID:         tx.NodeID,
+				StartTime:      tx.StartTime.Unix(),
+				EndTime:        tx.EndTime.Unix(),
+				Weight:         tx.Weight,
+				InputAddresses: strings.Split(tx.InputAddress, ","),
+			}
+		}
+		return stakers, nil
+	}
+	return utils.NewRouteHandler(handler, http.MethodPost, GetStakerRequest{}, []GetStakerResponse{})
 }
 
 func AddStakerRoutes(router utils.Router, ctx context.ServicesContext) {
@@ -51,4 +93,8 @@ func AddStakerRoutes(router utils.Router, ctx context.ServicesContext) {
 
 	delegatorSubrouter := router.WithPrefix("/delegators", "Staking")
 	delegatorSubrouter.AddRoute("/transactions", vr.listStakingTransactions(database.PChainAddDelegatorTx))
+
+	listStakersSubrouter := router.WithPrefix("/stakers", "Staking")
+	listStakersSubrouter.AddRoute("/list", vr.listStakers())
+
 }
